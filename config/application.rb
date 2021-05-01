196864fe6241f260cedcf2e6233cc18176763740
kawakami-masachika <supercell1023koeda@gmail.com>
2021-05-01 12:39:19 +0900

    タイムゾーンと日本語対応の設定追加

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,11 @@ Bundler.require(*Rails.groups)
 module EmployeeManagementApp
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators.system_tests = nil
     config.generators do |g|
